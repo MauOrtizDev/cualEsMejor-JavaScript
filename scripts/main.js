ce9e0bdd7287870000f380d9fbbce256f6e5504d
Mauricio Ortiz <mauortizdev@gmail.com>
2022-09-11 16:33:08 -0500

    Mejorando Estilos y agregando funciones de ordenar

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,9 @@ const pedirBaseDatos = async () => {
         productos.push(new Celular(...Object.values(el)))
     })
 
+    ordenarProductos();
     renderizarProductos();
+    mostrarListaBusqueda();
 }
 
 // Definición de una Clase Celular, con características y método de puntuación
@@ -45,7 +47,51 @@ DOMBotonComparar.addEventListener("click", () => window.open('./paginas/comparad
 
 pedirBaseDatos();
 
+function ordenarProductos() {
+    const seleccion = document.getElementById('ordenar');
+    seleccion.oninput = () => {
+        const caso = parseInt(seleccion.value);
+        switch (caso) {
+            case 0:
+                productos.splice(0, productos.length); console.log(productos);
+                pedirBaseDatos();
+                break;
+            case 1:
+                productos.sort(function (a, b) {
+                    return a.puntuacion - b.puntuacion
+                })
+                break;
+            case 2:
+                productos.sort(function (a, b) {
+                    return b.puntuacion - a.puntuacion
+                })
+                break;
+            case 3:
+                productos.sort(function (a, b) {
+                    return a.precio - b.precio
+                })
+                break;
+            case 4:
+                productos.sort(function (a, b) {
+                    return b.precio - a.precio
+                })
+                break;
+            case 5:
+                productos.sort(function (a,b){
+                    const fecha1 = a.fLanzamiento.split('/');
+                    const date1 = new Date(fecha1[2],fecha1[1]-1,fecha1[0]);
+                    const fecha2 = b.fLanzamiento.split('/');
+                    const date2 = new Date(fecha2[2],fecha2[1]-1,fecha2[0]);
+                    return date2 - date1
+                })
+                break;
+        }
+        renderizarProductos();
+    }
+}
+
 function renderizarProductos() {
+    DOMProductos.innerHTML = '';
     productos.forEach(el => {
         const miNodo = document.createElement('div');
         miNodo.classList.add('card', 'p-1');
@@ -141,7 +187,7 @@ function renderizarListaComp() {
         const h6 = document.createElement('h6');
         h6.textContent = `${el.nombre}`
         const botonBorrar = document.createElement('button');
-        botonBorrar.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        botonBorrar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
       </svg>`;
@@ -177,4 +223,40 @@ function borrarElemento(el) {
     console.log("Está en la posición " + posicion);
     productosAComparar.splice(posicion, 1);
     renderizarListaComp();
+}
+
+function mostrarListaBusqueda() {
+    const divLista = document.getElementById('resultados-busqueda');
+    const inputBuscar = document.getElementById('buscarCel');
+
+    inputBuscar.oninput = () => {
+        const arreglo = [];
+        const filtro = inputBuscar.value.toLowerCase();
+        divLista.innerHTML = '';
+
+        productos.forEach(el => {
+            if (el.nombre.toLowerCase().indexOf(filtro) > -1 && filtro != '') {
+                arreglo.push(el);
+                const li = document.createElement('li');
+                li.classList.add('list-item');
+                if (arreglo[0] == el) {
+                    li.classList.add('fw-bold')
+                }
+                console.log('si se da enter se elige ' + arreglo[0].nombre);
+                li.innerText = el.nombre;
+                li.addEventListener("click", function () { elegirParaComparacion(el) }, false)
+                divLista.append(li);
+
+            }
+        })
+
+        inputBuscar.onkeyup = function (tecla) {
+            if (tecla.key === 'Enter' || tecla.keyCode === 13) {
+
+                elegirParaComparacion(arreglo[0]);
+            }
+        }
     }
+
+}
+
